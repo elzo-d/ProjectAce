eed2c080c139fadd2ff1b90fdcb6f363fd39c4f7
Leon Smit <leon_94@live.nl>
2019-05-20 10:55:08 +0200

    Deleted debug for navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,17 @@ import { User } from './_models';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-    currentUser: User;
-    DEBUG: boolean = true;
+  currentUser: User;
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
