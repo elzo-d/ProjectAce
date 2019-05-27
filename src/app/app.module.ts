2c7e74415bb1aa83a08fd2d6b4b12b2588b8be2f
Thom van Dijk <thom.a.vandijk@gmail.com>
2019-05-27 12:45:06 +0200

    Removed node_modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 
-import { routing } from './app.routing';
-
-import { AlertComponent } from './_components';
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
-import { HomeComponent } from './home';
-import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
 import { HighscoresComponent } from './highscores/highscores.component';
-import { UserComponent } from './user/user.component';
 import { FriendlistComponent } from './friendlist/friendlist.component';
 import { CardHandComponent } from './card-hand/card-hand.component';
 import { GameComponent } from './game/game.component';
@@ -22,19 +14,13 @@ import { ChatboxComponent } from './friendlist/chatbox/chatbox.component';
 @NgModule({
     imports: [
         BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        routing,
-        OrderModule
+
     ],
     declarations: [
         AppComponent,
-        AlertComponent,
-        HomeComponent,
-        LoginComponent,
-        RegisterComponent,
+        
         HighscoresComponent,
-        UserComponent,
+       
         FriendlistComponent,
         CardHandComponent,
         GameComponent,
@@ -42,11 +28,6 @@ import { ChatboxComponent } from './friendlist/chatbox/chatbox.component';
         ChatboxComponent
     ],
     providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-        // provider used to create fake backend
-        fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
