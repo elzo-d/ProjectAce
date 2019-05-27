58006a3e03baee8fa90bc37607aa84435fa1f014
Thom van Dijk <thom.a.vandijk@gmail.com>
2019-05-27 13:04:18 +0200

    Added chatbox component to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
-
-import { HighscoresComponent } from './highscores/highscores.component';
-import { FriendlistComponent } from './friendlist/friendlist.component';
-import { CardHandComponent } from './card-hand/card-hand.component';
-import { GameComponent } from './game/game.component';
-import { CardTableComponent } from './card-table/card-table.component';
-import { ChatboxComponent } from './friendlist/chatbox/chatbox.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import {LoginComponent} from './login/login.component';
+import {GameComponent} from './game/game.component';
+import {CardHandComponent} from './card-hand/card-hand.component';
+import {CardTableComponent} from './card-table/card-table.component';
+import {HighscoresComponent} from './highscores/highscores.component';
+import {FriendlistComponent} from './friendlist/friendlist.component';
+import {OrderModule} from 'ngx-order-pipe';
+import {ChatboxComponent} from './friendlist/chatbox/chatbox.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-
-    ],
-    declarations: [
-        AppComponent,
-        
-        HighscoresComponent,
-       
-        FriendlistComponent,
-        CardHandComponent,
-        GameComponent,
-        CardTableComponent,
-        ChatboxComponent
-    ],
-    providers: [
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    GameComponent,
+    CardHandComponent,
+    CardTableComponent,
+    HighscoresComponent,
+    FriendlistComponent,
+    ChatboxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    OrderModule
+  ],
+  providers: [  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
