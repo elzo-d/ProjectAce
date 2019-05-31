35aba74a0e021cd98a9f24ac102a09d6d8c48c08
Jarco Uil <jarcouil@gmail.com>
2019-05-31 11:32:02 +0200

    fixed html/css voor friendlist etc

--- src/app/hidden/hidden.component.ts
+++ src/app/hidden/hidden.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth/auth.service'
 @Component({
   selector: 'app-hidden',
   templateUrl:'hidden.component.html',
+  styleUrls: ['./hidden.component.css']
 })
 export class HiddenComponent implements OnInit {
 
