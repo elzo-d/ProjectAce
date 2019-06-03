cfb6ecbbb918eacc42bc21d68b19358ee6015500
Jarco Uil <jarcouil@gmail.com>
2019-06-03 11:30:16 +0200

    added functionalities to the chatbox component

--- src/app/hidden/friendlist/chatbox/chatbox.component.ts
+++ src/app/hidden/friendlist/chatbox/chatbox.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatbox.component.css']
 })
 export class ChatboxComponent implements OnInit {
-
+  messages: Message[];
   constructor() { }
 
   ngOnInit() {
+    this.mockMessages();
   }
 
+  mockMessages() {
+    this.messages = [
+      { fromUser: true, text:"hi" },
+      { fromUser: false, text:"1" },
+      { fromUser: true, text:"hi" },
+      { fromUser: false, text:"2" },
+      { fromUser: true, text:"hi" },
+      { fromUser: false, text:"3" },
+      { fromUser: true, text:"hi" },
+      { fromUser: false, text:"4" },
+    ]
+  }
+
+  getStyle(user: boolean){
+    if(user){
+      return "me"
+    }
+    else{
+      return "other"
+    }
+  }
+
+  sendMessage(typedText: string): void {
+    this.messages.push(
+      {
+        fromUser: true,
+        text: typedText,
+      }
+    )
+  }
+}
+
+interface Message {
+  fromUser: boolean
+  text: string
 }
