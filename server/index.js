e789132c8dcd6f3324bf52baf40976faa6cb188c
Thom van Dijk <thom.a.vandijk@gmail.com>
2019-06-12 11:28:21 +0200

    Fixed chatbox server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
-const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const loginRoute = require("./routes/login.route");
 const hiddenRoute = require("./routes/hidden.route");
 
-// Express
-const app = express();
-const PORT = process.env.PORT || 5000;
+let express = require('express')
+let app = express();
 
 let http = require('http');
 let server = http.Server(app);
@@ -16,7 +14,18 @@ let server = http.Server(app);
 let socketIO = require('socket.io');
 let io = socketIO(server);
 
-//parse usual forms
+const port = process.env.PORT || 5000;
+
+// Chat socket
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('new-message', (message) => {
+    io.emit('new-message', message);
+  });
+});
+
+// Parse usual forms
 app.use(
   bodyParser.urlencoded({
     extended: true
@@ -28,16 +37,6 @@ app.use(bodyParser.json());
 app.use("/api/login", loginRoute);
 app.use("/api/hidden", hiddenRoute);
 
-// Chat socket
-io.on('connection', (socket) => {
-  console.log('User connected to chatbox');
-
-  socket.on('new-message', (message) => {
-    console.log("Message:" + message);
-    io.emit(message);
-  });
-});
-
-app.listen(PORT, () => {
-  console.log("Express starting listening on port " + PORT);
+server.listen(port, () => {
+  console.log(`started on port: ${port}`);
 });
