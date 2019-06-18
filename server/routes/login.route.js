1d8f6e499544de4e8341413177b3d9525d503303
Leon Smit <leon_94@live.nl>
2019-06-18 13:16:20 +0200

    Added password hashing

--- server/routes/login.route.js
+++ server/routes/login.route.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const _ = require("lodash");
 const loginRoutes = express.Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 let User = require("../models/User");
 
 const users = [
@@ -19,16 +22,25 @@ const signOptions = {
 
 const privateKey = fs.readFileSync("private.pem", "utf8");
 
-loginRoutes.route("/").post(function(req, res) {
+loginRoutes.route("/").post((req, res) => {
   console.log("login attempt");
+
+  let name, password;
+
   if (req.body.name && req.body.password) {
-    var name = req.body.name;
+    name = req.body.name;
+    password = req.body.password;
+  } else {
+    res
+      .status(400)
+      .json({ message: "Username or password not entered correctly" });
+    return;
   }
   console.log(`Login attempt with username: ${name}`);
 
   //!Testaccount
-  if (req.body.name == "user" && req.body.password == "user") {
-    console.log("testaccount");
+  if (name == "user" && password == "user") {
+    console.log("Testaccount logging in");
     let payload = { name, id: 1111 };
     let token = jwt.sign(payload, privateKey, signOptions);
     console.log(`>>> ${token}`);
@@ -43,38 +55,28 @@ loginRoutes.route("/").post(function(req, res) {
   //!End Testaccount
 
   User.findOne({ name: name }, "name password", (err, user) => {
-    //Handle error
+    //Error handling
     if (err) {
       res.status(401).json({ message: "no such user found" });
       return;
     }
 
-    console.log(user);
-
-    if (user && user.password === req.body.password) {
-      console.log("password match");
-      let payload = { name, id: user.id };
-      let token = jwt.sign(payload, privateKey, signOptions);
-      console.log(`>>> ${token}`);
-      res.json({
-        message: "ok",
-        token: token,
-        expiresIn: jwt.decode(token).exp,
-        user: user.name
-      });
-    } else {
-      console.log("no password match");
-      res.status(401).json({ message: "password did not match" });
-    }
+    //Password comparison
+    bcrypt.compare(password, user.password, function(err, passCorrect) {
+      if (passCorrect) {
+        let payload = { name, id: user.id };
+        let token = jwt.sign(payload, privateKey, signOptions);
+        res.json({
+          message: "ok",
+          token: token,
+          expiresIn: jwt.decode(token).exp,
+          user: user.name
+        });
+      } else {
+        res.status(401).json({ message: "password did not match" });
+      }
+    });
   });
-
-  // var user = users[_.findIndex(users, { name: name })];
-
-  // if (!user) {
-  //   console.log("user not found");
-  //   res.status(401).json({ message: "no such user found" });
-  //   return;
-  // }
 });
 
 module.exports = loginRoutes;
