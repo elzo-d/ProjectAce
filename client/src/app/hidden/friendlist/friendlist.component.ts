d05a60330449a6e83c358596a28e98fcc5af00b9
Thom van Dijk <thom.a.vandijk@gmail.com>
2019-06-19 10:53:08 +0200

    Fixed friendlist style

--- client/src/app/hidden/friendlist/friendlist.component.ts
+++ client/src/app/hidden/friendlist/friendlist.component.ts
@@ -27,7 +27,7 @@ export class FriendlistComponent implements OnInit {
 
   constructor(private orderPipe: OrderPipe, public auth: AuthService) {
     this.sortedFriends = orderPipe.transform(this.friends, 'id');
-    this.activeTab = ActiveTab.GENERAL_CHAT;
+    this.activeTab = ActiveTab.SEARCH;
   }
 
   get ActiveTab() { return ActiveTab; }
