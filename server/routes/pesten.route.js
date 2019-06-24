04960a1bf122c91cafa4b226c421ac6e0c6b1f64
Elzo Doornbos <32901471+elzo-d@users.noreply.github.com>
2019-06-24 12:58:42 +0200

    Progress in multiplayer 'pesten'

--- server/routes/pesten.route.js
+++ server/routes/pesten.route.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const pestenRoutes = express.Router();
+const {Pesten, Card, SUITS, MOVE_TYPES} = require("../games/pesten");
+
+let pesten;
 
 pestenRoutes.route("/").post((req, res) => {
   console.log("Post for pesten-API: req: " + JSON.stringify(req.body));
-  res.json({
-    error: "success",
-    message: "test"
-  });
+
+  switch(req.body.type) {
+    case 0: {
+      // starting game
+      pesten = new Pesten();
+      console.log("Started new 'pesten'-game");
+      res.json({
+        error: "success"
+      })
+      break;
+    }
+    case 1: {
+      // move
+      let c = new Card(1, 1);
+      c.setFromArray(req.body.card);
+      pesten.doUpdate(req.body.moveType, c, req.body.user);
+      console.log("Did update for 'pesten'-game");
+      res.json({
+        error: "success",
+        data: pesten.getState()
+      });
+      break;
+    }
+  }
+
 });
 
 module.exports = pestenRoutes;
