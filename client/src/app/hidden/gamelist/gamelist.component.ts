ccf9f256e4f060219670d4a635abb67d08e6c678
Elzo Doornbos <32901471+elzo-d@users.noreply.github.com>
2019-06-25 13:04:19 +0200

    'pesten' can now be played with 2 people via the server

--- client/src/app/hidden/gamelist/gamelist.component.ts
+++ client/src/app/hidden/gamelist/gamelist.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { GameDataService } from '../game-data.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../auth/auth.service';
 
 class ActiveGame {
   gameType:string;
   name:string;
+  players:number;
 }
 
 @Component({
@@ -14,23 +17,47 @@ class ActiveGame {
 })
 export class GamelistComponent implements OnInit {
 
-  activeGames:ActiveGame[] = [
-    {gameType: "Pesten", name: "Game 1"},
-    {gameType: "Pesten", name: "Game 2"},
-    {gameType: "KingMarker", name: "Game 3"},
-    {gameType: "KingMarker", name: "hh"},
-  ];
+  activeGames:ActiveGame[] = [];
 
   selectedGame:string;
 
+  finalList:ActiveGame[] = [];
+
   constructor(
     private gameDataService:GameDataService,
-    private router:Router
+    private router:Router,
+    private http:HttpClient,
+    private auth:AuthService
   ) { }
 
   ngOnInit() {
     this.selectedGame = this.gameDataService.selectedGame.value;
-    this.gameDataService.selectedGame$.subscribe(n => {this.selectedGame = n});
+
+    this.http.post("http://localhost:5000/api/pesten/list", {}).subscribe(
+      res => this.handleRequest(res),
+      err => console.log(err)
+    );
+
+    this.gameDataService.selectedGame$.subscribe(n => {
+      this.selectedGame = n;
+      this.getList();
+      this.http.post("http://localhost:5000/api/pesten/list", {}).subscribe(
+        res => this.handleRequest(res),
+        err => console.log(err)
+      );
+    });
+  }
+
+  handleRequest(res) {
+    this.activeGames = [];
+    for(let item of res.data.games) {
+      this.activeGames.push({
+        gameType: item.game,
+        name: item.hash,
+        players: item.players
+      });
+    }
+    this.getList();
   }
 
   getList() {
@@ -44,12 +71,22 @@ export class GamelistComponent implements OnInit {
         finalList.push(game);
       }
     }
-    return finalList;
+    this.finalList = finalList;
   }
 
   joinGame(game) {
     // join the game
     console.log("Joining " + game.name + " (type: " + game.gameType + ")");
+    this.http.post("http://localhost:5000/api/pesten", {
+      type: 2,
+      gameHash: game.name,
+      userId: this.auth.getId()
+    }).subscribe(
+      res => {
+        this.router.navigateByUrl("/hidden/pesten");
+      },
+      err => console.log(err)
+    );
   }
 
   joinRandom() {
@@ -61,10 +98,18 @@ export class GamelistComponent implements OnInit {
     }
   }
 
-  singlePlayer() {
-    // play singleplayer
-    console.log("Doing singleplayer game (type: " + this.selectedGame + ")");
-    this.router.navigateByUrl("/hidden/pesten");
+  newGame() {
+    // start new game
+    console.log("Starting new game (type: " + this.selectedGame + ")");
+    this.http.post("http://localhost:5000/api/pesten", {
+      type: 0,
+      userId: this.auth.getId()
+    }).subscribe(
+      res => {
+        this.router.navigateByUrl("/hidden/pesten");
+      },
+      err => console.log(err)
+    );
   }
 
   getTitle() {
